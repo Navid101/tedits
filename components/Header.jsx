a2c91bbf609862ea8b4bf1a3e699f549b086c714
Navid <navid.anjum@northsouth.edu>
2022-07-14 18:14:51 +0600

    logo change 2

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
         <div className='flex flex-col space-y-6 xl:space-y-0 '>
             <div className='h-12 w-12 xl:hidden block '>
-                <Image src="https://media.discordapp.net/attachments/235680551363674112/997039578043121674/000116154244_326803261677684_2838756276716891239_n.png?width=592&height=676" width="447px" height="521px" layout='responsive' alt='Logo' className='object-cover'></Image>
+                <img src="https://media.discordapp.net/attachments/235680551363674112/997039578043121674/000116154244_326803261677684_2838756276716891239_n.png?width=592&height=676" width="447px" height="521px" layout='responsive' alt='Logo' className='object-cover'></img>
             </div>
             <div className='flex  items-center w-full justify-between'>
                 <Link href="/"><a className='transition text-xl hover:text-fadeRed'>HOME</a></Link>
                 <Link href="/works"><a className='transition text-xl hover:text-fadeRed'>WORKS</a></Link>
                 <div className='h-12 w-12 hidden xl:block'>
-                    <Image src="https://media.discordapp.net/attachments/235680551363674112/997039578043121674/000116154244_326803261677684_2838756276716891239_n.png?width=592&height=676" width="447px" height="521px" layout='responsive' alt='Logo' className='object-cover'></Image>
+                    <img src="https://media.discordapp.net/attachments/235680551363674112/997039578043121674/000116154244_326803261677684_2838756276716891239_n.png?width=592&height=676" width="447px" height="521px" layout='responsive' alt='Logo' className='object-cover'></img>
                 </div>
                 <Link href="/packages"><a className='transition text-xl hover:text-fadeRed'>PACKAGES</a></Link>
                 <Link href="/contact"><a className='transition text-xl hover:text-fadeRed'>CONTACT</a></Link>
