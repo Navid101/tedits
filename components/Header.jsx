7e1282dc063134b1fd4151a2da4508d75e007c3d
Navid <navid.anjum@northsouth.edu>
2022-07-15 20:44:03 +0600

    logo fixed & text fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Header = () => {
   return (
         <div className='flex flex-col space-y-8 xl:space-y-0 '>
-            <div className='h-12 w-12 xl:hidden block '>
+            <div className='h-12 w-12 xl:hidden block self-center'>
                 <img src="https://media.discordapp.net/attachments/235680551363674112/997039578043121674/000116154244_326803261677684_2838756276716891239_n.png?width=592&height=676" width="447px" height="521px" layout='responsive' alt='Logo' className='object-cover'></img>
             </div>
             <div className='flex  items-center w-full justify-between'>
--- components/Item.jsx
+++ components/Item.jsx
@@ -5,12 +5,12 @@ const Item = ({item}) => {
     <div className='flex space-x-6 xl:space-x-24  items-center'>
         <img src={`/assets/${item.logo}.png`} className="h-[30px] md:h-[40px] xl:h-[50px]"></img>
         {!item.link? 
-        <h1 className='text-2xl'>{item.content}</h1>
+        <h1 className='text-xl xl:text-2xl'>{item.content}</h1>
         :
         item.logo==="twitter"?
-        <Link href={item.link}><a target="_blank" className='text-2xl'>{item.content}</a></Link>
+        <Link href={item.link}><a target="_blank" className='text-xl xl:text-2xl'>{item.content}</a></Link>
         :
-        <a className='text-2xl' href={`mailto:${item.link}.com`}>{item.link}</a>
+        <a className='text-xl xl:text-2xl' href={`mailto:${item.link}.com`}>{item.link}</a>
         }
     </div>
   )
