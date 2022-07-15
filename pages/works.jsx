368779b85178db741ee946dc46bf3a2895ea4911
Navid <navid.anjum@northsouth.edu>
2022-07-16 01:03:24 +0600

    sorted

--- pages/works.jsx
+++ pages/works.jsx
@@ -6,11 +6,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 const works = ({data}) => {
   const {works} = data
+  const newWorks = works.slice().sort((a, b) => b.rating - a.rating)
+
+  
   return (
     <div className="pt-12 xl:pt-24 flex flex-col space-y-24 items-center">
       <div className="grid xl:grid-cols-3 gap-10 ">
         {
-          works.map((item)=>{
+          newWorks.map((item)=>{
             return(
               <div className='' key={item.id}>
                 <Link href={item.url} passHref><a target="_blank"><Image src={Grablink(item.url,'max')} width="1280px" height="720px" className="object-cover transition duration-300 hover:scale-125"></Image></a></Link> 
@@ -33,6 +36,7 @@ export const getStaticProps = async () => {
         id
         url
         author
+        rating
       }
     }
     `
