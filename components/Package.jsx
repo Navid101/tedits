23104dd6ebeb1a1f1aa5f53852843cd37a889c49
Navid <navid.anjum@northsouth.edu>
2022-07-15 23:59:06 +0600

    color fixed

--- components/Package.jsx
+++ components/Package.jsx
@@ -5,7 +5,7 @@ const Package = ({pricing, title, contents}) => {
   return (
     <div className='flex flex-col items-center space-y-5'>
         <div className='border rounded'>
-            <h1 className={`text-lg font-medium text-center py-3`}>{title}</h1>
+            <h1 className={`text-lg font-medium text-center ${title} py-3`} >{title}</h1>
             {contents.map((content, index)=>{
                 return(
                     index%2?<h1 key={index} className='bg-paleWhite w-full text-center px-6 py-3'>{content}</h1>:<h1 key={index} className='bg-darkGray w-full text-center px-6 py-3'>{content}</h1>
